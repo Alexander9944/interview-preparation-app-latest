71774a1d67bbe1290ee91c4cb76523cbc28dcdd9
Alexander <anderalex789@gmail.com>
2026-04-24 16:38:17 +0530

    small tweaks

--- src/components/ScrollWrapper.jsx
+++ src/components/ScrollWrapper.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const ScrollWrapper = ({ children }) => {
   const effects = ['fade-up', 'zoom-in', 'fade-right'];
   const randomEffect = effects[Math.floor(Math.random() * effects.length)];
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
   return (
     <div data-aos={randomEffect}>
